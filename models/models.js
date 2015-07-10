235d1e02fe47b4a08025f6504fb29e3a848006ff
miriadaxdani <danivectorsf@gmail.com>
2015-07-10 21:26:27 +0200

    mod7 tema 3
    modelo de datos

--- models/models.js
+++ models/models.js
@@ -1,11 +1,35 @@
 var path = require('path');
 
+//Se preparan las variables, segun ejecutemos sqlite en local 
+//o postgress en heroku
+
+//esto es para poder ejecutar en local con npm start
+//ya que si ejecutamos npm start, no cogemos las variables de entorno .ent, y DATABASE_URL es undefined y da error en el match
+var getEnv = process.env.DATABASE_URL;
+if(!getEnv) getEnv="sqlite://:@:/";
+
+var url = getEnv.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name		= (url[6] || null);
+var user		= (url[2] || null);
+var pwd			= (url[3] || null);
+var protocol	= (url[1] || null);
+var dialect		= (url[1] || null);
+var port		= (url[5] || null);
+var host		= (url[4] || null);
+var storage		= process.env.DATABASE_STORAGE;
+
 //cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-//Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null,
-						{dialect: "sqlite", storage: "squiz-sqlite"}
+//Usar BBDD SQLite o postgress
+var sequelize = new Sequelize(DB_name, user, pwd,
+						{dialect: protocol, 
+						protocol: protocol,
+						port:	  port,
+						host:	  host,
+						storage:  storage,
+						omitNull: true
+						}
 					);
 					
 //Importar la definicion de la tabla Quiz en quiz.js
